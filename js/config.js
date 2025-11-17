179cea468b9ef96b493669a90653495c036741a5
Rishav Marasini <marasinirishav0@gmail.com>
2025-11-16 22:06:20 -0600

    Added API URL

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: '', // Leave empty since we are doing the fast track
+        userPoolClientId: '', // Leave empty
+        region: 'us-east-2'
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://h528vesuf2.execute-api.us-east-2.amazonaws.com/prod'
     }
 };
